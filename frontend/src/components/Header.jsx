e9e593dfa727547e21d258673e08c0746c31acae
SAICHAITANYAK2003 <saichaitanyakoduri2003@gmail.com>
2025-11-03 10:47:53 +0530

    ui update

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaAngleRight } from "react-icons/fa";
 import { BsDownload } from "react-icons/bs";
 import { motion } from "motion/react";
+import Typewriter from "typewriter-effect";
 
 const resumeLink =
   "https://drive.google.com/file/d/1p6ohFud6ekIF4_DwbPuwEJ9nsA-En0so/view?usp=sharing";
@@ -16,7 +17,6 @@ const Header = () => {
           duration: 0.4,
           scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
         }}
- 
       >
         <img
           src="https://res.cloudinary.com/saichaitanyacloudinary/image/upload/v1744732626/portfoliopic_uexofb.png"
@@ -27,13 +27,23 @@ const Header = () => {
       <h3 className="text-xl md:text-2xl mb-5 dark:text-white">
         Hi I’m Sai Chaitanya koduri <span className="">👋</span>
       </h3>
-      <h1 className="text-3xl sm:text-6xl lg:text-6xl mb-6 leading-16 dark:text-white">
-        Full Stack Web Developer.
+      <h1 className="text-3xl sm:text-4xl lg:text-5xl mb-5 leading-10 sm:leading-16 dark:text-white w-full max-w-2xl text-center">
+        <Typewriter
+          options={{
+            strings: [
+              "Cyber Security Engineer",
+              "Full Stack Developer",
+              "AI Enthusiast",
+            ],
+            autoStart: true,
+            loop: true,
+            delay:75,
+            deleteSpeed: 75,
+          }}
+        />
       </h1>
       <p className="  max-w-2xl mx-auto text-gray-500 dark:text-gray-400">
-        I'm a Full Stack Web Developer with experience in building websites
-        using the MERN stack. I'm ready to be hired and help build clean, fast,
-        and user-friendly web apps.
+        I’m a Cybersecurity Engineer and Full Stack Developer skilled in the MERN stack. I focus on building secure, fast, and user-friendly web applications with clean and efficient code.
       </p>
       <div className="flex flex-col md:flex-row md:items-center gap-3">
         <a
